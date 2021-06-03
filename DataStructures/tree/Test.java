0cdbfba376e8f9607ba82cf10266838defc4fd1b
ruan-luqingnian-r <15991875018@163.com>
2021-06-03 14:34:52 +0800

    tree Signed-off-by: ruan-luqingnian-r <15991875018@163.com>

---
+++ DataStructures/tree/Test.java
@@ -0,0 +1,23 @@
+package DataStructures.tree;
+
+import java.util.ArrayList;
+
+/**
+ * @PackgeName: DataStructures.tree
+ * @ClassName: Test
+ * @Author: 小天才
+ * Date: 2021/6/3 14:26
+ * project name: 算法和数据结构
+ * @Version: 0.0.1
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        /**
+         * 底层维护了Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA代码
+         *
+         */
+        ArrayList<Integer> integers = new ArrayList<>();
+    }
+}
