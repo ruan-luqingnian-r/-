db621ccd93b3417902a506a4c037472ea7e23250
ruan-luqingnian-r <15991875018@163.com>
2021-06-03 15:42:59 +0800

    tree/BinaryTreeDemo.java
    Signed-off-by: ruan-luqingnian-r <15991875018@163.com>

---
+++ DataStructures/tree/BinaryTreeDemo.java
@@ -0,0 +1,155 @@
+package DataStructures.tree;
+
+/**
+ * @PackgeName: DataStructures.tree
+ * @ClassName: BinaryTreeDemo
+ * @Author: 小天才
+ * Date: 2021/6/3 15:14
+ * project name: 算法和数据结构
+ * @Version: 0.0.1
+ * @Description: 二叉树
+ */
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+        //创建一颗二叉树
+        BinaryTree binaryTree = new BinaryTree();
+        //创建需要的节点
+        HeroNode root = new HeroNode(1, "宋江");
+        HeroNode node2 = new HeroNode(2, "吴用");
+        HeroNode node3 = new HeroNode(3, "卢俊义");
+        HeroNode node4 = new HeroNode(4, "林冲");
+        //先手动创建二叉树
+        root.setLeft(node2);
+        root.setRight(node3);
+        node3.setRight(node4);
+
+        binaryTree.setRoot(root);
+        //测试
+        System.out.println("前序遍历");
+        binaryTree.preOrder();
+        //测试
+        System.out.println("中序遍历");
+        binaryTree.infixOrder();
+        //测试
+        System.out.println("后序遍历");
+        binaryTree.postOrder();
+    }
+
+}
+//创建节点
+class HeroNode{
+    private int no;//序号
+    private String name;//名字
+    private HeroNode left;//左节点默认为空
+    private HeroNode right;//右节点默认为空
+    //创建构造器
+    public HeroNode(int no,String name){
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    //编写前序遍历方法
+    public void preOrder(){
+        System.out.println(this);//输出父节点
+        if (this.left != null){//向左递归
+            this.left.preOrder();
+        }
+        if (this.right != null){//向右递归
+            this.right.preOrder();
+        }
+    }
+
+    //中序遍历
+    public void infixOrder(){
+        if (this.left != null){
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if (this.right != null){
+            this.right.infixOrder();
+        }
+    }
+
+    //后续遍历
+    public void postOrder(){
+        if (this.left != null){
+            this.left.postOrder();
+        }
+        if (this.right != null){
+            this.right.postOrder();
+        }
+        System.out.println(this);
+    }
+}
+//创建二叉树
+class BinaryTree{
+    private HeroNode root;//创建根结点
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+    //前序遍历
+    public void preOrder(){
+        if (this.root != null){
+            this.root.preOrder();
+        }else {
+            System.out.println("二叉数为空");
+        }
+    }
+    //中序遍历
+    public void infixOrder(){
+        if (this.root != null){
+            this.root.infixOrder();
+        }else {
+            System.out.println("二叉树为空");
+        }
+    }
+    //后续遍历
+    public void postOrder(){
+        if (this.root != null){
+            this.root.postOrder();
+        }else {
+            System.out.println("二叉树为空");
+        }
+    }
+}
