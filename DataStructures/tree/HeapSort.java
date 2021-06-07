af5974ab7787897130680d99dddf3089c0daf84f
ruan-luqingnian-r <15991875018@163.com>
2021-06-07 12:59:03 +0800

    heapSort
    Signed-off-by: ruan-luqingnian-r <15991875018@163.com>

---
+++ DataStructures/tree/HeapSort.java
@@ -0,0 +1,26 @@
+package DataStructures.tree;
+
+/**
+ * @PackgeName: DataStructures.tree
+ * @ClassName: HeapSort
+ * @Author: 小天才
+ * Date: 2021/6/7 12:32
+ * project name: 算法和数据结构
+ * @Version: 0.0.1
+ * @Description: 堆排序
+ */
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] arr = {4,6,8,5,9};
+    }
+    /**
+     * 堆排序
+     */
+    public static void heapSort(int[] arr){
+        System.out.println("堆排序");
+    }
+    //将一共数组调整成大顶堆
+    public static void adjustHeap(int[] arr,int i,int length){
+
+    }
+}
