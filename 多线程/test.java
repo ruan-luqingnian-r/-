eb08b4ce4424d3d82c225ca846b4a47403176629
ruan-luqingnian-r <15991875018@163.com>
2021-06-27 18:44:06 +0800

    12123

---
+++ 多线程/test.java
@@ -0,0 +1,21 @@
+package 多线程;
+
+/**
+ * @PackgeName: 多线程
+ * @ClassName: test
+ * @Author: 小天才
+ * Date: 2021/6/27 18:42
+ * project name: Data-structure-and-algorithm
+ * @Version: 0.0.1
+ * @Description:
+ */
+public class test {
+
+
+    public static void main(String[] args) {
+
+        /**
+         * 死锁
+         */
+    }
+}
