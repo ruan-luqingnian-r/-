d761a8c6537e70645bf1ab54eebcad7d4037d10e
ruan-luqingnian-r <15991875018@163.com>
2021-06-28 09:56:23 +0800

    123123

---
+++ 多线程/frist/UnSafeThread.java
@@ -0,0 +1,48 @@
+package 多线程.frist;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @PackgeName: 多线程.frist
+ * @ClassName: UnSafeThread
+ * @Author: 小天才
+ * Date: 2021/6/27 19:09
+ * project name: Data-structure-and-algorithm
+ * @Version: 0.0.1
+ * @Description:
+ */
+public class UnSafeThread {
+
+    private static int num = 0;
+    private static CountDownLatch countDownLatch = new CountDownLatch(10);
+
+    /**
+     * 每次调用对num进行++操作
+     */
+    public static void inCreate(){
+        num++;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 100; j++) {
+                    inCreate();
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                countDownLatch.countDown();
+            }).start();
+        }
+        while (true){
+            if (countDownLatch.getCount() == 0){
+                System.out.println(num);
+                break;
+            }
+        }
+
+    }
+}
