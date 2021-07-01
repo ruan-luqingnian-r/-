25f49c76986503cf60a3df1d1fafab6ff6e97b1f
ruan-luqingnian-r <15991875018@163.com>
2021-07-01 09:08:22 +0800

    Service.java

--- NetWork/src/com/ruan/test03/Client.java
+++ NetWork/src/com/ruan/test03/Client.java
@@ -14,35 +14,75 @@ import java.util.Scanner;
  * @Description: 客户端
  */
 public class Client {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //创建套接字
-        Socket socket = new Socket("192.168.15.1", 8888);
-        //等待用户输入账号密码
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入用户名");
-        String userName = scanner.next();
-        System.out.println("请输入密码");
-        String pwd = scanner.next();
-        //组装用户对象
-        User user = new User(userName, pwd);
-        //使用对象流进行包装
-        OutputStream outputStream = socket.getOutputStream();
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-        objectOutputStream.writeObject(user);
-        //接收客户端返回的信息
-        InputStream inputStream = socket.getInputStream();
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        boolean flag = dataInputStream.readBoolean();
-        if (flag){
-            System.out.println("登录成功");
-        }else {
-            System.out.println("登录失败");
+        Socket socket = null;
+        OutputStream outputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        InputStream inputStream = null;
+        DataInputStream dataInputStream = null;
+        try {
+            socket = new Socket("192.168.15.1", 8888);
+            //等待用户输入账号密码
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入用户名");
+            String userName = scanner.next();
+            System.out.println("请输入密码");
+            String pwd = scanner.next();
+            //组装用户对象
+            User user = new User(userName, pwd);
+            //使用对象流进行包装
+            outputStream = socket.getOutputStream();
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(user);
+            //接收客户端返回的信息
+            inputStream = socket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+            boolean flag = dataInputStream.readBoolean();
+            if (flag){
+                System.out.println("登录成功");
+            }else {
+                System.out.println("登录失败");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭流
+            try {
+                if (outputStream != null){
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (objectOutputStream != null) {
+                    objectOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (dataInputStream != null) {
+                    dataInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        //关闭流
-        outputStream.close();
-        objectOutputStream.close();
-        inputStream.close();
-        dataInputStream.close();
-        socket.close();
     }
 }
--- NetWork/src/com/ruan/test03/Service.java
+++ NetWork/src/com/ruan/test03/Service.java
@@ -14,31 +14,79 @@ import java.net.Socket;
  * @Description: 服务端
  */
 public class Service {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) {
         //创建套接字
-        ServerSocket serverSocket = new ServerSocket(8888);
-        //等待接收数据
-        Socket accept = serverSocket.accept();
-        //获取客户端发送的信息
-        InputStream inputStream = accept.getInputStream();
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-        User user = (User) objectInputStream.readObject();
-        //进行数据校验
-        boolean flag = false;
-        if (user.getUsername().equals("小天才") && user.getPwd().equals("123456")){
-            flag = true;
+        ServerSocket serverSocket = null;
+        Socket accept = null;
+        InputStream inputStream = null;
+        ObjectInputStream objectInputStream = null;
+        OutputStream outputStream = null;
+        DataOutputStream dataOutputStream = null;
+        try {
+            serverSocket = new ServerSocket(8888);
+            //等待接收数据
+            accept = serverSocket.accept();
+            //获取客户端发送的信息
+            inputStream = accept.getInputStream();
+            objectInputStream = new ObjectInputStream(inputStream);
+            User user = (User) objectInputStream.readObject();
+            //进行数据校验
+            boolean flag = false;
+            if (user.getUsername().equals("小天才") && user.getPwd().equals("123456")){
+                flag = true;
+            }
+            //向客户端返回信息
+            outputStream = accept.getOutputStream();
+            dataOutputStream = new DataOutputStream(outputStream);
+            dataOutputStream.writeBoolean(flag);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            //关闭流
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+            if (objectInputStream != null) {
+                    objectInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (dataOutputStream != null) {
+                    dataOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (accept != null){
+                    accept.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        //向客户端返回信息
-        OutputStream outputStream = accept.getOutputStream();
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeBoolean(flag);
-        //关闭流
-        inputStream.close();
-        objectInputStream.close();
-        outputStream.close();
-        dataOutputStream.close();
-        accept.close();
-        serverSocket.close();
-
     }
 }
