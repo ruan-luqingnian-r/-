ddf565b2dbfc3ece3bd95fee2b989a1c04186e60
ruan-luqingnian-r <15991875018@163.com>
2021-07-02 10:12:48 +0800

    Send.java

---
+++ NetWork/src/com/ruan/test04/Send.java
@@ -0,0 +1,47 @@
+package com.ruan.test04;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ * @PackgeName: com.ruan.test04
+ * @ClassName: Send
+ * @Author: 小天才
+ * Date: 2021/7/1 12:54
+ * project name: Data-structure-and-algorithm
+ * @Version: 0.0.1
+ * @Description: 发送方
+ */
+public class Send {
+    public static void main(String[] args) throws IOException {
+        System.out.println("====发送方上线=====");
+        //1.准备套接字，指定发送的端口号
+
+        while (true) {
+            DatagramSocket datagramSocket = new DatagramSocket(8888);
+            Scanner scanner = new Scanner(System.in);
+            //2.准备数据包
+            String str = scanner.nextLine();
+            byte[] bytes = str.getBytes();
+            /**
+             * 待发送的数据包需要四个参数
+             * 1.待传输数据转为字节数组
+             * 2.字节数组的长度
+             * 3.封装接收方的ip
+             * 4.指定接收方的端口号
+             */
+            DatagramPacket datagramPacket = new DatagramPacket(bytes, bytes.length, InetAddress.getByName("localhost"), 9999);//数据包
+            //发送
+            datagramSocket.send(datagramPacket);
+            byte[] bytes1 = new byte[1024];
+            DatagramPacket datagramPacket1 = new DatagramPacket(bytes1, bytes.length);
+            datagramSocket.receive(datagramPacket1);
+            byte[] data = datagramPacket1.getData();
+            String s = new String(data, 0, datagramPacket1.getLength());
+            System.out.println("服务机:" + s);
+            //关闭流
+            datagramSocket.close();
+        }
+    }
+}
