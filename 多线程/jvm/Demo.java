2d3a28e3c9376392d457fd68b7a3ec56f8c6327c
ruan-luqingnian-r <15991875018@163.com>
2021-07-13 15:55:27 +0800

    123

--- 多线程/jvm/Demo.java
+++ 多线程/jvm/Demo.java
@@ -7,22 +7,36 @@ package 多线程.jvm;
  */
 public class Demo{
     static int x = 0,y = 0,a = 0,b = 0;
-
+    static int i = 0;
+    static boolean flag = true;
     public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            a = 1;
-            x = b;
-        });
-        Thread thread1 = new Thread(() -> {
-            b = 1;
-            y = a;
-        });
 
-        thread.start();
-        thread1.start();
-        thread.join();
-        thread.join();
 
-        System.out.println("x ======> " + x + "     y ======> " + y);
+        while (flag) {
+            Thread thread = new Thread(() -> {
+                a = 1;
+                x = b;
+            });
+            Thread thread1 = new Thread(() -> {
+                b = 1;
+                y = a;
+            });
+            i++;
+            thread.start();
+            thread1.start();
+            thread.join();
+            thread.join();
+            System.out.println("第"+ i + "次"+"x ======> " + x + "     y ======> " + y);
+            if (x == 0 && y == 0){
+                flag = false;
+            }else {
+                x = 0;
+                y = 0;
+                a = 0;
+                b = 0;
+            }
+        }
+
+
     }
 }
