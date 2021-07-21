e1e717fc09c877122835e8b74365d9a191d566eb
ruan-luqingnian-r <15991875018@163.com>
2021-07-21 10:42:42 +0800

    ArrayQueue.java

--- dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
+++ dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
@@ -14,7 +14,7 @@ class Queue{
     private int maxSize;//数组最大值
     private int front;//声明队列头部
     private int rear;//声明队列尾部
-
+    private  int[] arr;
     /**
      * 创建构造器
      */
@@ -22,7 +22,7 @@ class Queue{
         front = -1;
         rear = -1;
         this.maxSize = size;
-        int[] arr = new int[size];
+        arr = new int[size];
     }
     /**
      * 队列相关方法
@@ -34,6 +34,38 @@ class Queue{
     //是否空队列
     public boolean isNull(){
         return front == -1;
+    }
+    //入队列
+    public void add(int number){
+        //首先判断是否满
+        if (isFull()){
+            System.out.println("队列已满，无法入队");
+            return;
+        }
+        //如果未满则开始入队
+        rear++;
+        arr[rear] = number;
+    }
+    //查看队头（不出队列）
+    public int get(){
+        //先判断队列是否为空
+        if (isNull()){
+            System.out.println("队列为空，无数据");
+        }
+        return arr[front + 1];
+    }
+    //弹出队列头部数据
+    public int pop(){
+        //先判断队列是否为空
+        if (isNull()){
+            System.out.println("队列为空，无数据");
+        }
+        front++;
+        return arr[front];
+    }
+    //遍历队列
+    public void list(){
+
     }
 
 
