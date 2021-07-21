5367531d6b9ab7f265965f2525d4b1e40e1f6a89
ruan-luqingnian-r <15991875018@163.com>
2021-07-21 10:25:58 +0800

    ArrayQueue.java

--- dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
+++ dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
@@ -14,12 +14,27 @@ class Queue{
     private int maxSize;//数组最大值
     private int front;//声明队列头部
     private int rear;//声明队列尾部
-    //创建构造器
 
+    /**
+     * 创建构造器
+     */
     public Queue(int size) {
         front = -1;
         rear = -1;
         this.maxSize = size;
         int[] arr = new int[size];
     }
+    /**
+     * 队列相关方法
+     */
+    //是否满队列
+    public boolean isFull(){
+        return rear == maxSize - 1;
+    }
+    //是否空队列
+    public boolean isNull(){
+        return front == -1;
+    }
+
+
 }
