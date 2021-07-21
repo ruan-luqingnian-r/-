bf76b15d75827c1c0f4d0ee986bd304c5c47350c
ruan-luqingnian-r <15991875018@163.com>
2021-07-21 10:19:59 +0800

    ArrayQueue.java

--- dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
+++ dataStructuresAndAlgoorithm/dataStructures/queue/ArrayQueue.java
@@ -3,8 +3,23 @@ package dataStructuresAndAlgoorithm.dataStructures.queue;
 /**
  * @Author: ruan
  * Date: 2021/7/21 10:03
- * @Description: 数组模拟队列
+ * @Description: 数组模拟单向队列
+ * 队列特点先入先出
  */
 public class ArrayQueue {
 
+}
+//
+class Queue{
+    private int maxSize;//数组最大值
+    private int front;//声明队列头部
+    private int rear;//声明队列尾部
+    //创建构造器
+
+    public Queue(int size) {
+        front = -1;
+        rear = -1;
+        this.maxSize = size;
+        int[] arr = new int[size];
+    }
 }
