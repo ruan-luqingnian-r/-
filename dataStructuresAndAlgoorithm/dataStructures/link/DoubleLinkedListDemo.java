1a2419fdfc882796f75e46588e6f4e6ddce1349a
ruan-luqingnian-r <15991875018@163.com>
2021-07-22 10:16:17 +0800

    DoubleLinkedListDemo.java

--- dataStructuresAndAlgoorithm/dataStructures/link/DoubleLinkedListDemo.java
+++ dataStructuresAndAlgoorithm/dataStructures/link/DoubleLinkedListDemo.java
@@ -6,6 +6,28 @@ package dataStructuresAndAlgoorithm.dataStructures.link;
  * @Description: 双向链表
  */
 public class DoubleLinkedListDemo {
+    public static void main(String[] args) {
+        //测试
+        //创建节点
+        DoubleNode node1 = new DoubleNode(1, "赵");
+        DoubleNode node2 = new DoubleNode(2, "钱");
+        DoubleNode node3 = new DoubleNode(3, "孙");
+        DoubleNode node4 = new DoubleNode(4, "李");
+        //创建链表
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.ulAdd(node1);
+        doubleLinkedList.ulAdd(node2);
+        doubleLinkedList.ulAdd(node3);
+        doubleLinkedList.ulAdd(node4);
+        System.out.println("=========");
+        doubleLinkedList.list();
+        doubleLinkedList.delete(3);
+        System.out.println("=========");
+        doubleLinkedList.list();
+        doubleLinkedList.upDate(4,"哈哈");
+        System.out.println("=========");
+        doubleLinkedList.list();
+    }
 }
 /**
  * 定义节点
@@ -32,4 +54,94 @@ class DoubleNode{
                 ", name='" + name + '\'' +
                 '}';
     }
+}
+/**
+ * 定义链表
+ */
+class DoubleLinkedList{
+    //创建空数据的头节点
+    DoubleNode head = new DoubleNode(0,"");
+    /**
+     * 新增方法(无序)
+     */
+    public void ulAdd(DoubleNode newNode){
+        //创建辅助节点
+        DoubleNode temp = head;
+        //遍历链表
+        while (true){
+            if (temp.nextNode == null){
+                //找到链表尾部
+                break;
+            }
+            //节点后移
+            temp = temp.nextNode;
+        }
+        //将尾部链接新节点
+        temp.nextNode = newNode;
+        //新节点的上一个指向尾部
+        newNode.preNode = temp;
+    }
+    /**
+     * 删除链表
+     */
+    public void delete(int no){
+        DoubleNode temp =  head.nextNode;
+        boolean flag = false;
+        //遍历链表找到该节点
+        while (true){
+            if (temp == null){
+                break;
+            }
+            if (temp.id == no){
+                flag = true;
+                break;
+            }
+            temp = temp.nextNode;
+        }
+        if (flag){
+            //找到待删除节点
+            temp.preNode.nextNode = temp.nextNode;
+            temp.nextNode.preNode = temp.preNode;
+        }else {
+            System.out.println("无此节点");
+        }
+    }
+    /**
+     * 修改节点
+     */
+    public void upDate(int no,String name){
+        DoubleNode temp = head.nextNode;
+        boolean flag = false;
+        while (true){
+            if (temp == null){
+                break;
+            }
+            if (temp.id == no){
+                flag = true;
+                break;
+            }
+            temp = temp.nextNode;
+        }
+        if (flag){
+            temp.name = name;
+        }else {
+            System.out.println("无此节点");
+        }
+    }
+    /**
+     * 打印链表
+     */
+    public void list(){
+        DoubleNode temp = head;
+        while (true){
+            if (temp.nextNode == null){
+                break;
+            }
+            temp = temp.nextNode;
+            System.out.println(temp.toString());
+        }
+    }
+    /**
+     * 添加链表
+     */
 }
