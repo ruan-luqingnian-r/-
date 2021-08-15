d9c92c1c2845fde912b8a03da32ab18451d7be90
ruan-luqingnian-r <15991875018@163.com>
2021-08-15 17:32:40 +0800

    二分查找

--- dataStructuresAndAlgoorithm/dataStructures/search/Dichotomous.java
+++ dataStructuresAndAlgoorithm/dataStructures/search/Dichotomous.java
@@ -1,5 +1,7 @@
 package dataStructuresAndAlgoorithm.dataStructures.search;
 
+import java.util.ArrayList;
+
 /**
  * @Author: ruan
  * Date: 2021/8/15 17:19
@@ -8,5 +10,10 @@ package dataStructuresAndAlgoorithm.dataStructures.search;
 public class Dichotomous {
     public static void main(String[] args) {
         int[] arr = {2, 2, 2, 2, 21, 22, 31, 32, 32, 213, 312, 312, 312, 2112, 13214, 312312};
+    }
+
+
+    public static ArrayList<Integer> dichotomous(int[] arr,int target,int left,int right){
+
     }
 }
