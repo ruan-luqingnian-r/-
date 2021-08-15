b6c8e7cbf8327902133c33ae6723e3bc30a1bf6e
ruan-luqingnian-r <15991875018@163.com>
2021-08-15 17:19:57 +0800

    二分查找

---
+++ dataStructuresAndAlgoorithm/dataStructures/search/Dichotomous.java
@@ -0,0 +1,12 @@
+package dataStructuresAndAlgoorithm.dataStructures.search;
+
+/**
+ * @Author: ruan
+ * Date: 2021/8/15 17:19
+ * @Description: 二分查找
+ */
+public class Dichotomous {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 2, 2, 21, 22, 31, 32, 32, 213, 312, 312, 312, 2112, 13214, 312312};
+    }
+}
