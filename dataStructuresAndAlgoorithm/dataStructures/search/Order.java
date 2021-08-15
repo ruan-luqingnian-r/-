75ccfc93a1b7a655f764fc7008472a1a9783f319
ruan-luqingnian-r <15991875018@163.com>
2021-08-15 17:14:05 +0800

    Order.java

--- dataStructuresAndAlgoorithm/dataStructures/search/Order.java
+++ dataStructuresAndAlgoorithm/dataStructures/search/Order.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class Order {
     public static void main(String[] args) {
         int[] arr = {2, 2, 2, 2, 21, 22, 31, 32, 32, 213, 312, 312, 312, 2112, 13214, 312312};
-        ArrayList<Integer> order = order(arr, 2);
+        ArrayList<Integer> order = order(arr, 1);
         System.out.println(order);
     }
 
@@ -26,7 +26,7 @@ public class Order {
          */
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[0] == target){
+            if (arr[i] == target){
                 list.add(i);
             }
         }
