c06f8a2da8d63981c4dbae2128563f67fa37e416
ruan-luqingnian-r <15991875018@163.com>
2021-08-17 11:25:12 +0800

    Demo

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -1,5 +1,7 @@
 package dataStructuresAndAlgoorithm.dataStructures.demo;
 
+import java.util.HashMap;
+
 /**
  * @Author: ruan
  * Date: 2021/8/17 10:38
@@ -9,5 +11,13 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println(2 << 3);
+        System.out.println(40 >> 3);
+        swap(31,40);
+    }
+
+    public static void swap(int a,int b){
+        a = a + b;
+        b = a - b;
+        a = a - b;
     }
 }
