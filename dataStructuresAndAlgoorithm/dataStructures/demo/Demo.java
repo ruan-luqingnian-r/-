98280dc089abe70625e5005388f1f69018152ad6
ruan-luqingnian-r <15991875018@163.com>
2021-08-17 11:49:07 +0800

    Demo

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -12,14 +12,23 @@ public class Demo {
 
         System.out.println(2 << 3);
         System.out.println(40 >> 3);
-        swap(31,40);
+        System.out.printf("i++=%d,++i=%d",test1(),test2());
     }
 
     public static void swap(int a,int b){
         System.out.printf("a=%d,b=%d",a,b);
-        a = a^b;
-        b = b^a;
-        a = a^b;
+        a = a^b; //a = a^b
+        b = b^a; //b = b^a^b = a
+        a = a^b; //a = a^b^a = b
         System.out.printf("\na=%d,b=%d",a,b);
+    }
+
+    public static int test1(){
+        int i = 5;
+        return i++;
+    }
+    public static int test2(){
+        int i = 5;
+        return ++i;
     }
 }
