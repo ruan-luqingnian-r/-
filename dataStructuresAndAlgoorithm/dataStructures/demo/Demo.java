dd64718a8033b473d242e2e9103c7ec4b427bae5
ruan-luqingnian-r <15991875018@163.com>
2021-08-17 10:40:33 +0800

    Demo

---
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -0,0 +1,13 @@
+package dataStructuresAndAlgoorithm.dataStructures.demo;
+
+/**
+ * @Author: ruan
+ * Date: 2021/8/17 10:38
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println(2 << 3);
+    }
+}
