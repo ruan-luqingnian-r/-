e08b64e1e180fa450a81e44126049ae588b0cf04
ruan-luqingnian-r <15991875018@163.com>
2021-08-18 11:55:57 +0800

    Demo

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -1,5 +1,6 @@
 package dataStructuresAndAlgoorithm.dataStructures.demo;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -10,62 +11,26 @@ import java.util.HashMap;
  */
 public class Demo {
     public static void main(String[] args) {
-        int[] arr = {1,2,32,2,4,2,3,5,4,55,66,7,7,8,9,34,34,34,5,4,67,6,734,5456};
-        insertion(arr);
-        System.out.println(Arrays.toString(arr));
+
+        test();
 
     }
 
-    /**
-     * 冒泡排序
-     * @param arr
-     */
-    public static void bubble(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]){
-                    arr[j] = arr[j] ^ arr[j + 1];
-                    arr[j + 1] = arr[j + 1] ^ arr[j];
-                    arr[j] = arr[j] ^ arr[j + 1];
-                }
+    public static void test(){
+        try(FileInputStream fis = new FileInputStream("C:/Users/阮相歌/Desktop/Data-structure-and-algorithm/dataStructuresAndAlgoorithm/dataStructures/demo/test.txt");
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            FileOutputStream fos = new FileOutputStream("C:/Users/阮相歌/Desktop/Data-structure-and-algorithm/dataStructuresAndAlgoorithm/dataStructures/demo/copy.txt");
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+            ) {
+            //读取输入流
+            int size;
+            byte[] buf = new byte[1024];
+            while ((size = bis.read(buf)) != -1){
+                bos.write(buf,0,size);
             }
-        }
-    }
 
-    /**
-     * 选择排序
-     * @param arr
-     */
-    public static void select(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            int index = i;
-            int minValue = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (minValue > arr[j]){
-                    index = j;
-                    minValue = arr[j];
-                }
-            }
-            if (index != i){
-                arr[index] = arr[i];
-                arr[i] = minValue;
-            }
-        }
-    }
-
-    /**
-     * 插入排序
-     * @param arr
-     */
-    public static void insertion(int[] arr){
-        for (int i = 1; i < arr.length; i++) {
-            int value = arr[i];
-            int index = i - 1;
-            while (index >= 0 && value < arr[index]){
-                arr[index + 1] = arr[index];
-                index--;
-            }
-            arr[index + 1] = value;
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
