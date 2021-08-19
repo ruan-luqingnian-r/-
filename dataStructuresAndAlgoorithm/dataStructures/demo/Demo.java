c2b6836f0f0a0bb2c6325ee40dc2f9c2c14da2bf
ruan-luqingnian-r <15991875018@163.com>
2021-08-19 21:29:49 +0800

    Demo

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -2,7 +2,6 @@ package dataStructuresAndAlgoorithm.dataStructures.demo;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @Author: ruan
@@ -11,15 +10,16 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class Demo {
     public static void main(String[] args) {
-        //定义一个list用于存储目录
+        /*//定义一个list用于存储目录
         List<String> paths = new ArrayList<>();
         getAllFilePast(new File("C:\\Users\\阮相歌\\Desktop\\Data-structure-and-algorithm\\dataStructuresAndAlgoorithm\\dataStructures"),paths);
         for (String path : paths){
             System.out.println(path);
         }
-        List<Object> objects = Collections.synchronizedList(new ArrayList<>());
-        CopyOnWriteArrayList<String> strings = new CopyOnWriteArrayList<>();
-        strings.add("");
+        List<Object> objects = Collections.synchronizedList(new ArrayList<>());*/
+        int[] arr = {12,31,42,3,42,123,42,324,45,25,6,325,643,4,32545,65,34,21,12,3};
+        shell(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void getAllFilePast(File file, List<String> paths) {
@@ -35,6 +35,30 @@ public class Demo {
                 paths.add(f.getPath());
             }
         }
+    }
+
+    /**
+     * 希尔排序
+     * @param arr
+     */
+    private static void shell(int[] arr){
+        //分成两组
+        for (int gap = arr.length / 2;gap > 0;gap /= 2){
+            for (int i = gap;i < arr.length;i++){
+                //保存下标和待插入数据
+                int j = i;
+                int value = arr[j];
+                if (arr[j] < arr[j - gap]){
+                    while (j - gap >= 0 && arr[j] < arr[j - gap]){
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = value;
+                }
+
+            }
+        }
+
     }
 
 
