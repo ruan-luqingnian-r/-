fa63cd4de085cf324f09f13ab6f613ec087990fc
ruan-luqingnian-r <15991875018@163.com>
2021-08-19 21:41:25 +0800

    快速排序

--- dataStructuresAndAlgoorithm/dataStructures/sort/QuickSort.java
+++ dataStructuresAndAlgoorithm/dataStructures/sort/QuickSort.java
@@ -1,10 +1,19 @@
 package dataStructuresAndAlgoorithm.dataStructures.sort;
 
+import java.util.Arrays;
+
 /**
  * @Author: ruan
  * Date: 2021/8/19 21:40
  * @Description: 快速排序
  */
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {12,31,42,3,42,123,42,324,45,25,6,325,643,4,32545,65,34,21,12,3};
+        int[] sort = sort(arr);
+        System.out.println(Arrays.toString(sort));
+    }
 
+    private static int[] sort(int[] arr) {
+    }
 }
