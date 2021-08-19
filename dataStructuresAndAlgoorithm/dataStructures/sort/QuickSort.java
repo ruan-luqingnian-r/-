ed9e5a115401acda198ab10cc1e6b750c07e4f99
ruan-luqingnian-r <15991875018@163.com>
2021-08-19 22:00:39 +0800

    快速排序

--- dataStructuresAndAlgoorithm/dataStructures/sort/QuickSort.java
+++ dataStructuresAndAlgoorithm/dataStructures/sort/QuickSort.java
@@ -10,10 +10,51 @@ import java.util.Arrays;
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {12,31,42,3,42,123,42,324,45,25,6,325,643,4,32545,65,34,21,12,3};
-        int[] sort = sort(arr);
-        System.out.println(Arrays.toString(sort));
+        sort(arr,0,arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int[] sort(int[] arr) {
+    private static void sort(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int value = arr[(left + right) / 2];
+        int temp = 0;
+        while (l < r){
+            while (arr[l] < value){
+                l++;
+            }
+            while (arr[r] > value){
+                r--;
+            }
+            if (l >= r){
+                break;
+            }
+            //开始交换
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == value){
+                r--;
+            }
+            if (arr[r] == value){
+                l++;
+            }
+        }
+        if (l == r){
+            l++;
+            r--;
+        }
+        //向左递归
+        if (left < r){
+            sort(arr, left, r);
+        }
+        //向右递归
+        if (l < right){
+            sort(arr, l, right);
+        }
+
+
     }
+
+
 }
