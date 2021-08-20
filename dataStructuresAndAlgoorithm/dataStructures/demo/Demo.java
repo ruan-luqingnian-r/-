73e5dce167b28d2eb710de0a8cb56e976842addf
ruan-luqingnian-r <15991875018@163.com>
2021-08-20 22:25:43 +0800

    Demo

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -18,7 +18,7 @@ public class Demo {
         }
         List<Object> objects = Collections.synchronizedList(new ArrayList<>());*/
         int[] arr = {12,31,42,3,42,123,42,324,45,25,6,325,643,4,32545,65,34,21,12,3};
-        shellSort(arr);
+        quick(arr,0,arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -78,6 +78,51 @@ public class Demo {
                     arr[j] = value;
                 }
             }
+        }
+    }
+
+    /**
+     * 快速排序
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quick(int[] arr,int left,int right){
+        //保存下标和相关数据
+        int l = left;
+        int r = right;
+        int midValue = arr[(left + right) / 2];
+        int temp = 0;
+        while (l < r){
+            while (arr[l] < midValue){
+                l++;
+            }
+            while (arr[r] > midValue){
+                r--;
+            }
+            if (l >= r){
+                break;
+            }
+            //开始交换
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == midValue){
+                r--;
+            }
+            if (arr[r] == midValue){
+                l++;
+            }
+        }
+        if (l == r){
+            l++;
+            r--;
+        }
+        if (r > left){
+            quick(arr, left, r);
+        }
+        if (right > l){
+            quick(arr, l, right);
         }
     }
 
