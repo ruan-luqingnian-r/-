1e6c3cf6a540bc857a2bac649e5f004872b1710a
ruan-luqingnian-r <15991875018@163.com>
2021-08-23 15:59:58 +0800

    Thread01.java

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -3,6 +3,7 @@ package dataStructuresAndAlgoorithm.dataStructures.demo;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 /**
@@ -18,6 +19,15 @@ public class Demo {
         Thread thread = new Thread(futureTask);
         thread.setName("dome3");
         thread.start();
+        try {
+            System.out.println(futureTask.get());
+        } catch (InterruptedException e) {
+            //阻塞等待中被阻断
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            //执行过程中发送发生异常
+            e.printStackTrace();
+        }
     }
 
 }
