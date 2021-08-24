65bf6fc5e865a986aa872e4cf625e0154cb09bfc
ruan-luqingnian-r <15991875018@163.com>
2021-08-24 15:37:31 +0800

    Demo.java

--- dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/Demo.java
@@ -12,12 +12,7 @@ import java.util.concurrent.*;
 public class Demo {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
-        for (int i = 0; i < 10; i++) {
-            executorService.execute(new Thread04());
-        }
-        System.out.println("主线程名称:"+Thread.currentThread().getName());
-        executorService.shutdown();
+
 
     }
 
