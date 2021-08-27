1eab75c2b6965dfd341c4355b4ea39d8dd485cde
ruan-luqingnian-r <15991875018@163.com>
2021-08-27 19:57:15 +0800

    ThreadTest

--- dataStructuresAndAlgoorithm/dataStructures/demo/ThreadTest.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/ThreadTest.java
@@ -12,9 +12,7 @@ public class ThreadTest {
     private static volatile int num = 0;
     //使用lock，每个对象都是有锁的，只有获得这个锁才可以进行对应从操作
     static Lock lock = new ReentrantLock();
-
     public static void add() {
-
         lock.lock();
         try {
             num++;
@@ -22,17 +20,14 @@ public class ThreadTest {
             lock.unlock();
         }
     }
-
     //使用synchronized，和上述是同一个操作，这个是保证方法被锁住
     public synchronized void add2() {
         num++;
     }
-
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
-
                     add();
                 }
             });
