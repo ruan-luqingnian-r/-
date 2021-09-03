d3babe013a2db0a745234a3f93182035c334f050
ruan-luqingnian-r <15991875018@163.com>
2021-09-03 16:13:43 +0800

    Dome

--- dataStructuresAndAlgoorithm/dataStructures/demo/LockDome.java
+++ dataStructuresAndAlgoorithm/dataStructures/demo/LockDome.java
@@ -3,7 +3,7 @@ package dataStructuresAndAlgoorithm.dataStructures.demo;
 /**
  * @Author: ruan
  * Date: 2021/9/3 16:02
- * @Description:
+ * @Description: 死锁
  */
 public class LockDome {
     private static String lockA = "A";
