47d21e3a640a2a323a7b03f98ed2c7566dbc9725
ruan-luqingnian-r <15991875018@163.com>
2021-09-04 07:58:58 +0800

    UnReentrantLock

---
+++ dataStructuresAndAlgoorithm/dataStructures/demo/UnReentrantLock.java
@@ -0,0 +1,19 @@
+package dataStructuresAndAlgoorithm.dataStructures.demo;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author: ruan
+ * Date: 2021/9/3 20:45
+ * @Description: 不可重入锁
+ */
+public class UnReentrantLock {
+
+    public void lock(){
+        //判断是否已经被锁，如果被锁进入等待如果没有进行加锁
+    }
+
+    public void unlock(){
+
+    }
+}
