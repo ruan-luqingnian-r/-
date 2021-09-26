41942937fe5f8a8a4aec9ab4090b06efbdd6097c
ruan-luqingnian-r <15991875018@163.com>
2021-09-26 13:51:20 +0800

    练习

--- 练习/MyArrayList.java
+++ 练习/MyArrayList.java
@@ -36,6 +36,10 @@ public class MyArrayList implements Serializable {
         this.elementData = EMPTY_ELEMENT_DATA;
     }
 
+    /**
+     * 有参构造
+     * @param initialCapacity
+     */
     public MyArrayList(int initialCapacity){
         if (initialCapacity == 0){
             this.elementData = EMPTY_ELEMENT_DATA;
@@ -44,5 +48,137 @@ public class MyArrayList implements Serializable {
         }else {
             throw new IllegalArgumentException("参数错误");
         }
+    }
+
+    /**
+     * 扩容机制
+     * @param minCapacity 最小容量
+     */
+    public void ensureCapacityInternal(int minCapacity){
+        //判断是否是第一次扩容
+        if (elementData == EMPTY_ELEMENT_DATA){
+            //如果是第一次扩容，使用默认容量或者最小容量
+            minCapacity = Math.max(DEFAULT_CAPACITY,minCapacity);
+        }
+        //如果不是第一次扩容，判断是否需要扩容
+        if (elementData.length - minCapacity < 0){
+            //需要扩容
+            int oldCapacity = elementData.length;
+            int newCapacity = oldCapacity + (oldCapacity >> 1);
+            //比较扩容出来的值和最小值的大小
+            if (newCapacity - minCapacity < 0){
+                newCapacity = minCapacity;
+            }
+            //创建新数组
+            Object[] objects = new Object[newCapacity];
+            //拷贝数据
+            System.arraycopy(elementData,0,objects,0,size);
+            //修改引用
+            elementData = objects;
+        }
+    }
+
+    /**
+     * 检查下标是否越界
+     * @param index 数组下标
+     */
+    public void rangeCheck(int index){
+        if (index < 0 || index > size){
+            throw new IndexOutOfBoundsException("下标越界");
+        }
+    }
+
+    /**
+     * 添加数据
+     * @param o 待添加数据
+     * @return
+     */
+    public boolean add(Object o){
+        //并发判断
+        modCount++;
+        //容量判断
+        ensureCapacityInternal(size + 1);
+        //插入数据
+        elementData[size++] = o;
+        return true;
+    }
+
+    /**
+     * 根据下标获取数据
+     * @param index 数组下标
+     * @return
+     */
+    public Object get(int index){
+        //下标检查
+        rangeCheck(index);
+        return elementData[index];
+    }
+
+    /**
+     * 根据对象获取下标
+     * @param o 对象
+     * @return 下标
+     */
+    public int indexOf(Object o){
+        if (o == null){
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null){
+                    return i;
+                }
+            }
+        }else {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据下标修改对象
+     * @param index 要修改的下标
+     * @param o 新值
+     * @return 旧值
+     */
+    public Object set(int index,Object o){
+        //并发判断
+        modCount++;
+        //检查下标
+        rangeCheck(index);
+        Object odlValue = elementData[index];
+        elementData[index] = o;
+        return odlValue;
+    }
+
+    /**
+     * 根据下标删除索引
+     * @param index 下标
+     * @return 删除的值
+     */
+    public Object remove(int index){
+        //并发判断
+        modCount++;
+        //检查下标
+        rangeCheck(index);
+        Object odlValue = elementData[index];
+        //计算待删除的下标后还有多少元素
+        int munMove = size - index - 1;
+        if (munMove > 0){
+            //移动数据
+            System.arraycopy(elementData,index + 1,elementData,index,munMove);
+        }
+        //末位置空
+        elementData[--size] = null;
+        return odlValue;
+    }
+
+    /**
+     * 获取数组大小
+     * @return 数组大小
+     */
+    public int size(){
+        return this.size;
     }
 }
