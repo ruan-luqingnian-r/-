752586966d7b71a5ff84d1a6e08564a99906276a
ruan-luqingnian-r <15991875018@163.com>
2021-09-29 12:38:03 +0800

    test

--- 练习/java927/Sort.java
+++ 练习/java927/Sort.java
@@ -15,11 +15,24 @@ public class Sort {
             arr[i] = (int) (Math.random() * 1000 + 1);
         }
         System.out.println("排序前:" + Arrays.toString(arr));
+        bubbleSort(arr);
         System.out.println("排序后:" + Arrays.toString(arr));
     }
 
+    /**
+     * 冒泡排序
+     * @param arr
+     */
     public static void bubbleSort(int[] arr){
-
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]){
+                    arr[j] = arr[j] ^ arr[j + 1];
+                    arr[j + 1] = arr[j + 1] ^ arr[j];
+                    arr[j] = arr[j] ^ arr[j + 1];
+                }
+            }
+        }
     }
 
 
