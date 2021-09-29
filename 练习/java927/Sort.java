0759526d5781abac6b2dc566fe84a0c4f9fd59d3
ruan-luqingnian-r <15991875018@163.com>
2021-09-29 12:49:31 +0800

    练习

--- 练习/java927/Sort.java
+++ 练习/java927/Sort.java
@@ -15,7 +15,7 @@ public class Sort {
             arr[i] = (int) (Math.random() * 1000 + 1);
         }
         System.out.println("排序前:" + Arrays.toString(arr));
-        bubbleSort(arr);
+        selectSort(arr);
         System.out.println("排序后:" + Arrays.toString(arr));
     }
 
@@ -33,6 +33,37 @@ public class Sort {
                 }
             }
         }
+    }
+
+    /**
+     * 选择排序
+     * @param arr
+     */
+    public static void selectSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            //保存下标
+            int minIndex = i;
+            int minValue = arr[minIndex];
+            //遍历数组找到真正的最小值
+            for (int j = i + 1; j < arr.length; j++) {
+                if (minValue > arr[j]){
+                    minIndex = j;
+                    minValue = arr[j];
+                }
+            }
+            if (i != minIndex){
+                arr[minIndex] = arr[i];
+                arr[i] = minValue;
+            }
+        }
+    }
+
+    /**
+     * 插入排序
+     * @param arr
+     */
+    public static void insertionSort(int[] arr){
+
     }
 
 
