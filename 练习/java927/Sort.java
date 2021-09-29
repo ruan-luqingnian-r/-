a918182af5b52cf9d12e60b8be0eca1c9f0b994c
ruan-luqingnian-r <15991875018@163.com>
2021-09-29 12:31:35 +0800

    test

--- 练习/java927/Sort.java
+++ 练习/java927/Sort.java
@@ -15,48 +15,7 @@ public class Sort {
             arr[i] = (int) (Math.random() * 1000 + 1);
         }
         System.out.println("排序前:" + Arrays.toString(arr));
-        selectSort(arr);
         System.out.println("排序后:" + Arrays.toString(arr));
     }
 
-    /**
-     * 冒泡排序
-     * @param arr
-     */
-    public static void bubbleSort(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]){
-                    arr[j] = arr[j] ^ arr[j + 1];
-                    arr[j + 1] = arr[j + 1] ^ arr[j];
-                    arr[j] = arr[j] ^ arr[j + 1];
-                }
-            }
-        }
-    }
-
-
-    /**
-     * 选择排序
-     * @param arr
-     */
-    public static void selectSort(int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            //保存数据和下标
-            int minIndex = i;
-            int minValue = arr[minIndex];
-            //遍历数组找到真正的最小值
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < minValue){
-                    minIndex = j;
-                    minValue = arr[j];
-                }
-            }
-            if (minIndex != i){
-                arr[minIndex] = arr[i];
-                arr[i] = minValue;
-            }
-        }
-
-    }
 }
