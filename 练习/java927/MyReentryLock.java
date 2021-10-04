df4d4f2839bb3fa5e7bdb173c1f42e86bd2191bd
ruan-luqingnian-r <15991875018@163.com>
2021-10-04 17:10:39 +0800

    test

--- 练习/java927/MyReentryLock.java
+++ 练习/java927/MyReentryLock.java
@@ -15,13 +15,33 @@ public class MyReentryLock {
     /**
      * 持锁线程
      */
-    private Thread thread = null;
+    private Thread lockOrder = null;
 
     /**
      * 加锁次数
      */
     private int modCount = 0;
 
+    /**
+     * 加锁方法
+     */
+    public synchronized void lock(){
+        System.out.println("开始加锁...");
+        //获取当前线程
+        Thread thread = Thread.currentThread();
+        while (isLock && lockOrder != thread){
+            System.out.println("进入等待");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isLock = true;
+        lockOrder = thread;
+        modCount++;
+        System.out.println("加锁成功");
+    }
 
 
 
