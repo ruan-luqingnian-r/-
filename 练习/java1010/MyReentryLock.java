1aeff6de8ee53837bb41a34237b2219424ec1857
ruan-luqingnian-r <15991875018@163.com>
2021-10-10 09:59:04 +0800

    test

--- 练习/java1010/MyReentryLock.java
+++ 练习/java1010/MyReentryLock.java
@@ -23,6 +23,37 @@ public class MyReentryLock {
      * 加锁
      */
     public synchronized void lock(){
-        //获取持锁线程
+        System.out.println("开始加锁...");
+        Thread thread = Thread.currentThread();
+        while (isLock && thread != lockOrder){
+            System.out.println("进入等待...");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        isLock = true;
+        lockOrder = thread;
+        codCount++;
+        System.out.println("加锁完成");
+    }
+
+    /**
+     * 解锁
+     */
+    public synchronized void unlock(){
+        System.out.println("开始解锁...");
+        Thread thread = Thread.currentThread();
+        while (thread == lockOrder){
+            System.out.println("进入解锁..");
+            codCount--;
+            if (codCount == 0){
+                isLock = false;
+                lockOrder = null;
+                notify();
+                System.out.println("解锁成功");
+            }
+        }
     }
 }
